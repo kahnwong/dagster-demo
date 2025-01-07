5bc130b555e4fb48a8c56010829d2950a188e0dc
Karn Wong <karn@karnwong.me>
2025-01-07 11:13:39 +0700

    get asset return from downstream asset

--- dags/assets/bronze/foo/assets.py
+++ dags/assets/bronze/foo/assets.py
@@ -2,6 +2,7 @@ from dagster import asset
 from dagster import DailyPartitionsDefinition
 from dagster import load_assets_from_current_module
 
+
 daily_partitions_def = DailyPartitionsDefinition(
     start_date="2023-06-01", timezone="Asia/Bangkok"
 )
@@ -19,9 +20,9 @@ def a(context):
     key_prefix=["bronze", "foo"], group_name="foo", partitions_def=daily_partitions_def
 )
 def b(context):
-    context.log.info(f"foo: {context.asset_partition_key_for_output()}")
+    context.log.info(f"foo: {context.partition_key}")
 
-    return [1, 2, 3]
+    return [1, 2, context.partition_key]
 
 
 assets = load_assets_from_current_module()
--- dags/assets/silver/foo/assets.py
+++ dags/assets/silver/foo/assets.py
@@ -1,26 +1,36 @@
+from typing import Any
+
 from dagster import asset
-from dagster import AssetIn
+from dagster import AssetDep
+from dagster import AssetExecutionContext
+from dagster import DailyPartitionsDefinition
 from dagster import load_assets_from_current_module
 from dagster import TimeWindowPartitionMapping
-from dagster import WeeklyPartitionsDefinition
 
-weekly_partitions_def = WeeklyPartitionsDefinition(
+from dags.assets.bronze.foo.assets import b
+
+
+daily_partitions_def = DailyPartitionsDefinition(
     start_date="2023-06-01", timezone="Asia/Bangkok"
 )
 
 
 @asset(
-    partitions_def=weekly_partitions_def,
-    ins={
-        "a": AssetIn(
-            key=["bronze", "foo", "b"],
+    partitions_def=daily_partitions_def,
+    deps=[
+        AssetDep(
+            b,
             partition_mapping=TimeWindowPartitionMapping(),
         )
-    },
+    ],
     key_prefix=["silver", "foo"],
     group_name="foo",
 )
-def e(a):
+def e(context: AssetExecutionContext, b: Any):
+    partition = context.partition_key
+    context.log.info(f"partition: {partition}")
+    context.log.info(f"b: {b}")
+
     return [1, 2, 3]
 
 
